3c322acf6463c23d031579ba371987fa8b14d36a
mervinq <136318917+mervinq@users.noreply.github.com>
2023-06-14 19:21:23 +0530

    rlazy_cpption.py

--- plugins/rlazy_cpption.py
+++ plugins/rlazy_cpption.py
@@ -1,32 +1,30 @@
-    # Credit @LazyDeveloper.
-    # Please Don't remove credit.
-        # Born to make history @LazyDeveloper !
-
-    # Thank you LazyDeveloper for helping us in this Journey
-from pyrogram import Client, filters 
+from pyrogram import Client, filters
 from database.users_chats_db import db
 
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Note: Lazy_Mode active ✅\n\n__𝙶𝚒𝚟𝚎 𝚖𝚎 𝚊 𝚌𝚊𝚙𝚝𝚒𝚘𝚗 𝚝𝚘 𝚜𝚎𝚝.__\n\n𝙴𝚡𝚊𝚖𝚙𝚕𝚎:- `/set_caption {filename}\n\n💾 Size: {filesize}\n\n⏰ Duration: {duration}`**")
+        return await message.reply_text(
+            "⚠️ **Note: Mode active ✅**\n\n𝙶𝚒𝚟𝚎 𝚖𝚎 𝚊 𝚌𝚊𝚙𝚝𝚒𝚘𝚗 𝚝𝚘 𝚜𝚎𝚝.\n\n📝 **Example:** `/set_caption {filename}`"
+        )
     caption = message.text.split(" ", 1)[1]
     await db.set_caption(message.from_user.id, caption=caption)
-    await message.reply_text("__** 𝚈𝙾𝚄𝚁 𝙲𝙰𝙿𝚃𝙸𝙾𝙽 𝚂𝙰𝚅𝙴𝙳 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 ✅**__")
+    await message.reply_text("✅️ **Your Caption has been saved successfully!**")
 
-    
+
 @Client.on_message(filters.private & filters.command('del_caption'))
 async def delete_caption(client, message):
-    caption = await db.get_caption(message.from_user.id)  
+    caption = await db.get_caption(message.from_user.id)
     if not caption:
-       return await message.reply_text("Note: Lazy_Mode active ✅\n\n😔**Sorry sweetheart ! No Caption found...**😔")
+        return await message.reply_text("⚠️ **Note: Mode active ✅**\n\n😔**Sorry, no caption found...**😔")
     await db.set_caption(message.from_user.id, caption=None)
-    await message.reply_text("**** Your Caption deleted successfully**✅️")
-                                       
+    await message.reply_text("✅️ **Your Caption has been deleted successfully!**")
+
+
 @Client.on_message(filters.private & filters.command('see_caption'))
 async def see_caption(client, message):
-    caption = await db.get_caption(message.from_user.id)  
+    caption = await db.get_caption(message.from_user.id)
     if caption:
-       await message.reply_text(f"**Note: Lazy_Mode active ✅\n\nYour Caption:-**\n\n`{caption}`")
+        await message.reply_text(f"⚠️ **Note: Mode active ✅**\n\n**Your Caption:**\n\n`{caption}`")
     else:
-       await message.reply_text("😔**Sorry ! No Caption found...**😔")
+        await message.reply_text("😔**Sorry, no caption found...**😔")
