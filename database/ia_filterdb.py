b125efe0c500d47b840bb514c2de040fcf10872d
mervinq <136318917+mervinq@users.noreply.github.com>
2023-06-14 15:52:04 +0530

    ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -12,10 +12,10 @@ from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTE
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-
 client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]
 instance = Instance.from_db(db)
+
 
 @instance.register
 class Media(Document):
@@ -28,16 +28,15 @@ class Media(Document):
     caption = fields.StrField(allow_none=True)
 
     class Meta:
-        indexes = ('$file_name', )
+        indexes = ('$file_name',)
         collection_name = COLLECTION_NAME
 
 
 async def save_file(media):
-    """Save file in database"""
-
-    # TODO: Find better way to get same file_id for same media to avoid duplicates
+    """Save file in the database"""
     file_id, file_ref = unpack_new_file_id(media.file_id)
     file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
+    
     try:
         file = Media(
             file_id=file_id,
@@ -49,31 +48,25 @@ async def save_file(media):
             caption=media.caption.html if media.caption else None,
         )
     except ValidationError:
-        logger.exception('Error occurred while saving file in database')
+        logger.exception('Error occurred while saving the file in the database')
         return False, 2
     else:
         try:
             await file.commit()
-        except DuplicateKeyError:      
+        except DuplicateKeyError:
             logger.warning(
-                f'{getattr(media, "file_name", "NO_FILE")} is already saved in database'
+                f'{getattr(media, "file_name", "NO_FILE")} is already saved in the database'
             )
-
             return False, 0
         else:
-            logger.info(f'{getattr(media, "file_name", "NO_FILE")} is saved to database')
+            logger.info(f'{getattr(media, "file_name", "NO_FILE")} is saved to the database')
             return True, 1
 
 
-
 async def get_search_results(query, file_type=None, max_results=10, offset=0, filter=False):
-    """For given query return (results, next_offset)"""
-
+    """Return (results, next_offset) for the given query"""
     query = query.strip()
-    #if filter:
-        #better ?
-        #query = query.replace(' ', r'(\s|\.|\+|\-|_)')
-        #raw_pattern = r'(\s|_|\-|\.|\+)' + query + r'(\s|_|\-|\.|\+)'
+
     if not query:
         raw_pattern = '.'
     elif ' ' not in query:
@@ -101,22 +94,18 @@ async def get_search_results(query, file_type=None, max_results=10, offset=0, fi
         next_offset = ''
 
     cursor = Media.find(filter)
-    # Sort by recent
     cursor.sort('$natural', -1)
-    # Slice files according to offset and max results
     cursor.skip(offset).limit(max_results)
-    # Get list of files
     files = await cursor.to_list(length=max_results)
 
     return files, next_offset, total_results
 
 
-
 async def get_file_details(query):
     filter = {'file_id': query}
     cursor = Media.find(filter)
-    filedetails = await cursor.to_list(length=1)
-    return filedetails
+    file_details = await cursor.to_list(length=1)
+    return file_details
 
 
 def encode_file_id(s: bytes) -> str:
