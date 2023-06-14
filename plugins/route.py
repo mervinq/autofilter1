ee963ff97b98a03676dc82ac079eea31d3b2068a
mervinq <136318917+mervinq@users.noreply.github.com>
2023-06-14 16:11:06 +0530

    route.py

--- plugins/route.py
+++ plugins/route.py
@@ -4,4 +4,10 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("in & as LazyDeveloper...")
+    return web.json_response("in & as Tesla ...")  # Replace [Your Name] with the appropriate name you want to use
+
+app = web.Application()
+app.router.add_routes(routes)
+
+if __name__ == "__main__":
+    web.run_app(app)
