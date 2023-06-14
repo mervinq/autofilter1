54ea46eb452a6b05c246123711d5c36f710381c7
mervinq <136318917+mervinq@users.noreply.github.com>
2023-06-14 16:21:13 +0530

    Update rlazy_filedetect.py

--- plugins/rlazy_filedetect.py
+++ plugins/rlazy_filedetect.py
@@ -1,8 +1,3 @@
-    # Credit @LazyDeveloper.
-    # Please Don't remove credit.
-        # Born to make history @LazyDeveloper !
-
-    # Thank you LazyDeveloper for helping us in this Journey
 from pyrogram import Client, filters
 from pyrogram.enums import MessageMediaType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
@@ -10,12 +5,18 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 
 @Client.on_message(filters.private & filters.reply)
 async def refunc(client, message):
-    if (message.reply_to_message.reply_markup) and isinstance(message.reply_to_message.reply_markup, ForceReply):
+    if (
+        message.reply_to_message.reply_markup
+        and isinstance(message.reply_to_message.reply_markup, ForceReply)
+    ):
         new_name = message.text
         await message.delete()
-        await message.delete()
         media = await client.get_messages(message.chat.id, message.reply_to_message.id)
-        file = media.reply_to_message.document or media.reply_to_message.video or media.reply_to_message.audio
+        file = (
+            media.reply_to_message.document
+            or media.reply_to_message.video
+            or media.reply_to_message.audio
+        )
         filename = file.file_name
         types = file.mime_type.split("/")
         mime = types[0]
@@ -27,18 +28,32 @@ async def refunc(client, message):
             out_filename = new_name
             await message.reply_to_message.delete()
             if mime == "video":
-                markup = InlineKeyboardMarkup([[
-                    InlineKeyboardButton("📁 Document", callback_data="upload_document"),
-                    InlineKeyboardButton("🎥 Video", callback_data="upload_video")]])
+                markup = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📁 Document", callback_data="upload_document"),
+                            InlineKeyboardButton("🎥 Video", callback_data="upload_video"),
+                        ]
+                    ]
+                )
             elif mime == "audio":
-                markup = InlineKeyboardMarkup([[InlineKeyboardButton(
-                    "📁 Document", callback_data="doc"), InlineKeyboardButton("🎵 audio", callback_data="upload_audio")]])
+                markup = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📁 Document", callback_data="doc"),
+                            InlineKeyboardButton("🎵 Audio", callback_data="upload_audio"),
+                        ]
+                    ]
+                )
             else:
                 markup = InlineKeyboardMarkup(
-                    [[InlineKeyboardButton("📁 Document", callback_data="upload_document")]])
-            # Lazy-WarninG -> Please Dont chnage anything after this Line 
-            await message.reply_text(f"**Select the output file type**\n**🎞New Name** :- ```{out_filename}```", reply_to_message_id=mg_id, reply_markup=markup)
-
+                    [[InlineKeyboardButton("📁 Document", callback_data="upload_document")]]
+                )
+            await message.reply_text(
+                f"**Select the output file type**\n**🎞 New Name** :- ```{out_filename}```",
+                reply_to_message_id=mg_id,
+                reply_markup=markup,
+            )
         except:
             try:
                 out = filename.split(".")
@@ -46,18 +61,36 @@ async def refunc(client, message):
                 out_filename = new_name + "." + out_name
             except:
                 await message.reply_to_message.delete()
-                await message.reply_text("**Error** :  No  Extension in File, Not Supporting", reply_to_message_id=mg_id)
+                await message.reply_text(
+                    "**Error**: No extension in the file, not supporting",
+                    reply_to_message_id=mg_id,
+                )
                 return
             await message.reply_to_message.delete()
             if mime == "video":
-                markup = InlineKeyboardMarkup([[InlineKeyboardButton(
-                    "📁 Document", callback_data="upload_document"), InlineKeyboardButton("🎥 Video", callback_data="upload_video")]])
+                markup = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📁 Document", callback_data="upload_document"),
+                            InlineKeyboardButton("🎥 Video", callback_data="upload_video"),
+                        ]
+                    ]
+                )
             elif mime == "audio":
-                markup = InlineKeyboardMarkup([[InlineKeyboardButton(
-                    "📁 Document", callback_data="upload_document"), InlineKeyboardButton("🎵 audio", callback_data="upload_audio")]])
+                markup = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📁 Document", callback_data="upload_document"),
+                            InlineKeyboardButton("🎵 Audio", callback_data="upload_audio"),
+                        ]
+                    ]
+                )
             else:
                 markup = InlineKeyboardMarkup(
-                    [[InlineKeyboardButton("📁 Document", callback_data="upload_document")]])
-            # Lazy-WarninG -> Please Dont chnage anything after this Line 
-            await message.reply_text(f"**Select the output file type**\n**🎞New Name** :- ```{out_filename}```",
-                                     reply_to_message_id=mg_id, reply_markup=markup)
+                    [[InlineKeyboardButton("📁 Document", callback_data="upload_document")]]
+                )
+            await message.reply_text(
+                f"**Select the output file type**\n**🎞 New Name** :- ```{out_filename}```",
+                reply_to_message_id=mg_id,
+                reply_markup=markup,
+            )
